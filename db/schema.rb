5a319ccea29e15543dd2c96aa0f909cf91b055bf
kongo <d.parshenko@gmail.com>
2014-05-30 13:50:38 +0300

    Added Picture model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140530104713) do
+ActiveRecord::Schema.define(version: 20140530104921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20140530104713) do
   create_table "albums", force: true do |t|
     t.integer  "user_id"
     t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "pictures", force: true do |t|
+    t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
