854fa8bfec2f0489916b2742c86c10cdf71221cc
kongo <d.parshenko@gmail.com>
2014-05-30 14:02:52 +0300

    Picture must belong to Album, Album must belong to user

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,4 +1,5 @@
 class Album < ActiveRecord::Base
   belongs_to :user
   has_many :pictures
+  validates_presence_of :user
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :album
+  validates_presence_of :album
   mount_uploader :image, ImageUploader
 end
