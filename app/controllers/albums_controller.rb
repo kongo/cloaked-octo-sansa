13acece571d2f8e7e759df02031d169d4e25c7a9
kongo <d.parshenko@gmail.com>
2014-05-30 14:49:26 +0300

    Create album functionality

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,4 +5,12 @@ class AlbumsController < ApplicationController
     @albums = current_user.albums
   end
 
+  def create
+    a = Album.new(params[:album].permit(:title))
+    a.user = current_user
+    a.save!
+    flash[:success] = "Album created"
+    redirect_to albums_path
+  end
+
 end
